001a6383588e08d68c5fe7167125b1eee831ccad
Cy Kirsch <cykirsch@gmail.com>
2020-09-14 10:34:34 -0500

    Finish testing config service  - agent disabling cli only  - default configuration  - version resolver

--- tests/unit/Services/ApmConfigServiceTest.php
+++ tests/unit/Services/ApmConfigServiceTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use AG\ElasticApmLaravel\Contracts\VersionResolver;
 use AG\ElasticApmLaravel\Services\ApmConfigService;
 use Codeception\Test\Unit;
 use Illuminate\Config\Repository as ConfigRepository;
@@ -7,13 +8,7 @@ use Illuminate\Contracts\Foundation\Application;
 
 class ApmConfigServiceTest extends Unit
 {
-    private const SPAN_NAME = 'test-measure';
-
     private $appMock;
-    private $configMock;
-    private $eventsMock;
-
-    private $collectorService;
 
     protected function setUp(): void
     {
@@ -58,7 +53,102 @@ class ApmConfigServiceTest extends Unit
         $this->assertTrue($this->configService->isAgentDisabled());
     }
 
-    /*
-     * More tests to come, gathering some initial feedback first.
-     */
+    public function testIsAgentDisabledCli()
+    {
+        $this->repoMock->shouldReceive('get')
+            ->once()
+            ->with('elastic-apm-laravel.active')
+            ->andReturn(true);
+        $this->repoMock->shouldReceive('get')
+            ->once()
+            ->with('elastic-apm-laravel.cli.active')
+            ->andReturn(false);
+
+        $this->assertTrue($this->configService->isAgentDisabled());
+    }
+
+    public function testIsAgentDisabledNot()
+    {
+        $this->repoMock->shouldReceive('get')
+            ->once()
+            ->with('elastic-apm-laravel.active')
+            ->andReturn(true);
+        $this->repoMock->shouldReceive('get')
+            ->once()
+            ->with('elastic-apm-laravel.cli.active')
+            ->andReturn(true);
+
+        $this->assertFalse($this->configService->isAgentDisabled());
+    }
+
+    public function testGetAgentConfig()
+    {
+        $this->appMock->shouldReceive('version')
+            ->once()
+            ->andReturn('3.2.1');
+        $this->appMock->shouldReceive('bound')
+            ->once()
+            ->with(VersionResolver::class)
+            ->andReturn(false);
+
+        $packageConfig = require __DIR__ . '/../../../config/elastic-apm-laravel.php';
+        $configService = new ApmConfigService(
+            $this->appMock,
+            new ConfigRepository(['elastic-apm-laravel' => $packageConfig])
+        );
+
+        $this->assertEquals(
+            [
+                'framework' => 'Laravel',
+                'frameworkVersion' => '3.2.1',
+                'active' => false,
+                'httpClient' => [],
+                'appName' => 'Laravel',
+                'appVersion' => '',
+                'env' => [
+                    'DOCUMENT_ROOT',
+                    'REMOTE_ADDR',
+                ],
+                'environment' => 'development',
+                'serverUrl' => 'http://127.0.0.1:8200',
+                'secretToken' => null,
+                'hostname' => gethostname(),
+            ],
+            $configService->getAgentConfig()
+        );
+    }
+
+    public function testVersionResolver()
+    {
+        $this->appMock->shouldReceive('version')
+            ->once()
+            ->andReturn('3.2.1');
+        $this->appMock->shouldReceive('bound')
+            ->once()
+            ->with(VersionResolver::class)
+            ->andReturn(true);
+        $this->appMock->shouldReceive('make')
+            ->once()
+            ->with(VersionResolver::class)
+            ->andReturn(new VersionResolverDouble());
+
+        $packageConfig = require __DIR__ . '/../../../config/elastic-apm-laravel.php';
+        $configService = new ApmConfigService(
+            $this->appMock,
+            new ConfigRepository(['elastic-apm-laravel' => $packageConfig])
+        );
+
+        $this->assertEquals(
+            '1.2.3',
+            $configService->getAgentConfig()['appVersion']
+        );
+    }
+}
+
+class VersionResolverDouble implements VersionResolver
+{
+    public function getVersion(): string
+    {
+        return '1.2.3';
+    }
 }
