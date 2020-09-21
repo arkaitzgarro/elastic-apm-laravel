4ae35d05b56494f0d895bb26a4644f22def5b379
Arkaitz Garro <arkaitz.garro@gmail.com>
2020-09-21 14:27:28 +0200

    chore: remove RequestStartTime references from AgentBuilder

--- src/AgentBuilder.php
+++ src/AgentBuilder.php
@@ -3,7 +3,6 @@
 namespace AG\ElasticApmLaravel;
 
 use AG\ElasticApmLaravel\Collectors\EventDataCollector;
-use AG\ElasticApmLaravel\Collectors\RequestStartTime;
 use Illuminate\Support\Collection;
 use Nipwaayoni\AgentBuilder as NipwaayoniAgentBuilder;
 use Nipwaayoni\ApmAgent;
@@ -15,18 +14,8 @@ use Nipwaayoni\Stores\TransactionsStore;
 
 class AgentBuilder extends NipwaayoniAgentBuilder
 {
-    /** @var RequestStartTime */
-    private $start_time;
-
     /** @var Collection */
     private $collectors;
-
-    public function withRequestStartTime(RequestStartTime $start_time): self
-    {
-        $this->start_time = $start_time;
-
-        return $this;
-    }
 
     public function withEventCollectors(Collection $collectors): self
     {
@@ -42,15 +31,11 @@ class AgentBuilder extends NipwaayoniAgentBuilder
         EventFactoryInterface $event_factory,
         TransactionsStore $transactions_store): ApmAgent
     {
-        if (null === $this->start_time) {
-            $this->start_time = new RequestStartTime(microtime(true));
-        }
-
         if (null === $this->collectors) {
             $this->collectors = new Collection();
         }
 
-        $agent = new Agent($config, $shared_context, $connector, $event_factory, $transactions_store, $this->start_time);
+        $agent = new Agent($config, $shared_context, $connector, $event_factory, $transactions_store);
 
         $this->collectors->each(function (EventDataCollector $collector) use ($agent) {
             $agent->addCollector($collector);
